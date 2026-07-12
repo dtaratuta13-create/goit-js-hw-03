ca25bf71a1270fd72afe6c1c7e43ca1d9452d5b2
TaratutaDmytro <dtaratuta13@gmail.com>
2026-07-13 00:59:08 +0200

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,11 @@
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  const spamWords = ["spam", "sale", "offer", "urgent", "free"];
-  for (const word of spamWords) {
-    if (lowerCaseMessage.includes(word)) {
-      return true;
+function filterArray(numbers, value) {
+  const result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      result.push(numbers[i]);
     }
   }
-  return false;
-}
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+  return result;
+}
